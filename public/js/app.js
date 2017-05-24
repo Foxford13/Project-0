887db5625dba07fae90515b806d0fc6e4b7a1570
Jakub Dziekan <jdziek89@gmail.com>
2017-05-24 14:48:10 +0100

    almost done

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,91 @@
-"use strict";console.log("woof");
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiWUFBQUEsU0FBUUMsSUFBSSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnd29vZicpO1xuIl19
+$(() => {
+  const $isPlaying = $('.current');
+  let playerOnePlaying = true;
+  const boardSize = 64;
+  $isPlaying.text('Player 1 - Black').addClass('current-one');
+  const $scoreOne = $('.score-one');
+  const $scoreTwo = $('.score-two');
+  const $game = $('.game');
+  const $resetBtn =   $('.reset-rev');
+  let $boxes = null;
+
+  function startingPieces () {
+    $boxes.eq(27).addClass('yellow');
+    $boxes.eq(28).addClass('black');
+    $boxes.eq(35).addClass('black');
+    $boxes.eq(36).addClass('yellow');
+  }
+
+  function scoreCounter() {
+    $scoreOne.text( 'Player One' +' ' + $('.black').length);
+    $scoreTwo.text('Player Two' +' ' + $('.yellow').length);
+  }
+
+  function startingPosition() {
+    for ( let i = 0; i < boardSize; i++) {
+      $game.append($('<div>').addClass('box'));
+    }
+    $boxes = $('.box');
+    startingPieces();
+    scoreCounter();
+  }
+  startingPosition();
+
+  function reset() {
+    $boxes.removeClass('black').removeClass('yellow');
+    startingPieces();
+    scoreCounter();
+  }
+  $resetBtn.on('click', reset);
+
+  function fillBoxes(e, classToAdd, classToRemove, boxNumber) {
+    const $square = $(e.target);
+    const index = $square.index();
+    const squaresToTurn = [];
+    let i = boxNumber;
+
+    let j = 8;
+    let k = -8;
+    if (boxNumber === 8) {
+      while ($boxes.eq(index + j).hasClass(classToRemove)) {
+        squaresToTurn.push(index + j);
+        j += 8;
+      }
+    } else if (boxNumber === - 8) {
+      while ($boxes.eq(index + k).hasClass(classToRemove)) {
+
+        squaresToTurn.push(index + k);
+        k -= 8;
+      }
+    }
+
+    while ($boxes.eq(index + i).hasClass(classToRemove) && (index + i)%8 !== 7 && (index + i)%8 !== 0) {
+      squaresToTurn.push(index + i);
+      i += boxNumber;
+    }
+
+    const lastFilledDivInASequence = squaresToTurn[squaresToTurn.length - 1] + boxNumber;
+
+    if ($boxes.eq(lastFilledDivInASequence).hasClass(classToAdd) && lastFilledDivInASequence >= 0 && lastFilledDivInASequence <= 63) {
+      for (let i = 0; i < squaresToTurn.length; i++) {
+        $square.addClass(classToAdd);
+        $('body').find($boxes).eq(squaresToTurn[i]).removeClass(classToRemove).addClass(classToAdd);
+        playerOnePlaying = classToAdd === 'black' ? false : true;
+        playerOnePlaying ? $isPlaying.text('Player 1 - Black').addClass('current-one').removeClass('current-two') : $isPlaying.text('Player 2 - yellow').addClass('current-two').removeClass('current-one');
+      }
+    }
+  }
+
+
+  function turn(e) {
+
+    const classToAdd = playerOnePlaying ? 'black' : 'yellow';
+    const classToRemove = playerOnePlaying ? 'yellow' : 'black';
+    if ($(e.target).hasClass(classToAdd)  || $(e.target).hasClass(classToRemove)) return false;
+    [1,9,8,7,-1,-9,-8,-7].forEach((number) => {
+      fillBoxes(e, classToAdd, classToRemove, number);
+    });
+    scoreCounter();
+  }
+  $boxes.on('click', turn);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,48 @@
 console.log('woof');
+$(() => {
+  console.log('asasa');
+
+  const $boxes = $('.box');
+  const $isPlaying = $('.current');
+  let playerOnePlaying = true;
+  var myArray = new Array();
+
+  $isPlaying.text('Player 1');
+
+  $('.box').each(function(i) {
+    $(this).text(i);
+
+  });
+
+  $boxes.each(arrayOfSquares);
+
+  function arrayOfSquares(index, box) {
+    if ($(box).hasClass('black')) {
+      myArray.push('black');
+    }else if ($(box).hasClass('yellow')) {
+      myArray.push('yellow');
+    } else {
+      myArray.push(undefined);
+    }
+  }
+
+  function alternateMatches(e){
+    myArray = new Array();
+    if(playerOnePlaying){
+      $(e.target).addClass('black');
+      playerOnePlaying = false;
+      $isPlaying.text('Player 2');
+      console.log(playerOnePlaying);
+    } else if(!playerOnePlaying){
+      $(e.target).addClass('yellow');
+      playerOnePlaying = true;
+      console.log(playerOnePlaying);
+      $isPlaying.text('Player 1');
+    }
+    $boxes.each(arrayOfSquares);
+    console.log(myArray);
+  }
+  $boxes.one('click', alternateMatches);
+
+
+});
