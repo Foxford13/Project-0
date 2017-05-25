f6a8600e147757036c11e23b71bddbee839c4d1b
Jakub Dziekan <jdziek89@gmail.com>
2017-05-25 10:06:08 +0100

    improved naming conventions

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,18 @@
 $(() => {
   const $isPlaying = $('.current');
   let playerOnePlaying = true;
-  const boardSize = 64;
-  $isPlaying.text('Player 1 - Black').addClass('current-one');
+  const boardSize = 8*8;
   const $scoreOne = $('.score-one');
   const $scoreTwo = $('.score-two');
   const $game = $('.game');
   const $resetBtn =   $('.reset-rev');
-  let $boxes = null;
+  let $squares = null;
 
   function startingPieces () {
-    $boxes.eq(27).addClass('yellow');
-    $boxes.eq(28).addClass('black');
-    $boxes.eq(35).addClass('black');
-    $boxes.eq(36).addClass('yellow');
+    $squares.eq(27).addClass('yellow');
+    $squares.eq(28).addClass('black');
+    $squares.eq(35).addClass('black');
+    $squares.eq(36).addClass('yellow');
   }
 
   function scoreCounter() {
@@ -23,69 +22,61 @@ $(() => {
 
   function startingPosition() {
     for ( let i = 0; i < boardSize; i++) {
-      $game.append($('<div>').addClass('box'));
+      $game.append($('<div>').addClass('square'));
     }
-    $boxes = $('.box');
+    $squares = $('.square');
     startingPieces();
     scoreCounter();
+    $isPlaying.text('Player 1 - Black').addClass('current-one');
   }
   startingPosition();
 
   function reset() {
-    $boxes.removeClass('black').removeClass('yellow');
+    $squares.removeClass('black').removeClass('yellow');
     startingPieces();
     scoreCounter();
   }
   $resetBtn.on('click', reset);
 
-  function fillBoxes(e, classToAdd, classToRemove, boxNumber) {
+  function fillsquares(e, piecesInPlay, enemyPieces, piecesCoordinates) {
     const $square = $(e.target);
     const index = $square.index();
-    const squaresToTurn = [];
-    let i = boxNumber;
+    const piecesToTurn = [];
+    let i = piecesCoordinates;
 
-    let j = 8;
-    let k = -8;
-    if (boxNumber === 8) {
-      while ($boxes.eq(index + j).hasClass(classToRemove)) {
-        squaresToTurn.push(index + j);
-        j += 8;
-      }
-    } else if (boxNumber === - 8) {
-      while ($boxes.eq(index + k).hasClass(classToRemove)) {
-
-        squaresToTurn.push(index + k);
-        k -= 8;
+    if (piecesCoordinates === 8 || piecesCoordinates === -8) {
+      while ($squares.eq(index + i).hasClass(enemyPieces)) {
+        piecesToTurn.push(index + i);
+        i += piecesCoordinates;
       }
     }
 
-    while ($boxes.eq(index + i).hasClass(classToRemove) && (index + i)%8 !== 7 && (index + i)%8 !== 0) {
-      squaresToTurn.push(index + i);
-      i += boxNumber;
+    while ($squares.eq(index + i).hasClass(enemyPieces) && (index + i)%8 !== 7 && (index + i)%8 !== 0) {
+      piecesToTurn.push(index + i);
+      i += piecesCoordinates;
     }
 
-    const lastFilledDivInASequence = squaresToTurn[squaresToTurn.length - 1] + boxNumber;
+    const pieceFlankingEnemyPieces = piecesToTurn[piecesToTurn.length - 1] + piecesCoordinates;
 
-    if ($boxes.eq(lastFilledDivInASequence).hasClass(classToAdd) && lastFilledDivInASequence >= 0 && lastFilledDivInASequence <= 63) {
-      for (let i = 0; i < squaresToTurn.length; i++) {
-        $square.addClass(classToAdd);
-        $('body').find($boxes).eq(squaresToTurn[i]).removeClass(classToRemove).addClass(classToAdd);
-        playerOnePlaying = classToAdd === 'black' ? false : true;
+    if ($squares.eq(pieceFlankingEnemyPieces).hasClass(piecesInPlay) && pieceFlankingEnemyPieces >= 0 && pieceFlankingEnemyPieces <= 63) {
+      for (let i = 0; i < piecesToTurn.length; i++) {
+        $square.addClass(piecesInPlay);
+        $('body').find($squares).eq(piecesToTurn[i]).removeClass(enemyPieces).addClass(piecesInPlay);
+        playerOnePlaying = piecesInPlay === 'black' ? false : true;
         playerOnePlaying ? $isPlaying.text('Player 1 - Black').addClass('current-one').removeClass('current-two') : $isPlaying.text('Player 2 - yellow').addClass('current-two').removeClass('current-one');
       }
     }
   }
 
-
   function turn(e) {
 
-    const classToAdd = playerOnePlaying ? 'black' : 'yellow';
-    const classToRemove = playerOnePlaying ? 'yellow' : 'black';
-    if ($(e.target).hasClass(classToAdd)  || $(e.target).hasClass(classToRemove)) return false;
+    const piecesInPlay = playerOnePlaying ? 'black' : 'yellow';
+    const enemyPieces = playerOnePlaying ? 'yellow' : 'black';
+    if ($(e.target).hasClass(piecesInPlay)  || $(e.target).hasClass(enemyPieces)) return false;
     [1,9,8,7,-1,-9,-8,-7].forEach((number) => {
-      fillBoxes(e, classToAdd, classToRemove, number);
+      fillsquares(e, piecesInPlay, enemyPieces, number);
     });
     scoreCounter();
   }
-  $boxes.on('click', turn);
+  $squares.on('click', turn);
 });
